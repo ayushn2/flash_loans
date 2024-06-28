383cfa44cf765138df841b237848b05208df7ec3
Ayush Nainwal <aayu@Ayushs-Air.bbrouter>
2024-06-29 01:02:34 +0530

    Created a method to deposit tokens to the pool

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -1,8 +1,25 @@
-// SPDX-License-Identifier :ISC
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./Token.sol";
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract FlashLoan{
+    using SafeMath for uint256;
 
+    Token public token;
+    uint256 public poolBalance;
+
+    constructor(address _tokenAddress){
+
+        token = Token(_tokenAddress);
+    }
+
+    function depositTokens(uint256 _amount) external{
+
+        require(_amount > 0,"Must deposit at least one token");
+        token.transferFrom(msg.sender, address(this), _amount);
+        poolBalance = poolBalance.add(_amount);
+    }
 }
--- test/FlashLoan.js
+++ test/FlashLoan.js
@@ -1,9 +1,11 @@
 const {expect} = require('chai')
 const {ethers} = require('hardhat');
 
-const tokens = (n) =>{
-    return ethers.utils.parseUnits(n,toString(),'ether')
-}
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+  }
+
+let token,flashLoan;
 
 const ether = tokens
 
@@ -20,12 +22,25 @@ describe('FlashLoan',()=>{
         const FlashLoanReciever = await ethers.getContractFactory('FlashLoanReceiver')
         const Token = await ethers.getContractFactory('Token')
 
+        // Deploy Token
         token = await Token.deploy('Ayush Nainwal','Aaya','1000000')
+
+        //  Deploy Flash Loan Pool
+        flashLoan = await FlashLoan.deploy(token.address)
+
+        // Approve tokens before depositing
+        let transaction = await token.connect(deployer).approve(flashLoan.address, tokens(1000000))
+        await transaction.wait()
+        
+
+        // Deposit tokens to the pool
+        transaction = await flashLoan.connect(deployer).depositTokens(tokens(1000000))
+        await transaction.wait()
     })
 
     describe('Deployment',()=>{
-        it('works',()=>{
-            expect(1+1).to.equal(2)
+        it('sends tokens to flash loan pool contract', async ()=>{
+            expect(await token.balanceOf(flashLoan.address)).to.equal(tokens(1000000))
         })
     })
 })
